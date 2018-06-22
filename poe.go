851b850a559ab59c936fe2bcc813cdd5905a7df3
Jon Penn <Jon@qbox.io>
2018-06-22 10:39:18 -0500

    Update poe.go

--- poe.go
+++ poe.go
@@ -3,97 +3,97 @@ package jsonport
 import "github.com/qbox-io/jgh"
 
 func (j Json)  PoeArray() (ret []Json) {
-	ret, err = j.Array()
+	ret, err := j.Array()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeBool() (ret bool) {
-	ret, err = j.Bool()
+	ret, err := j.Bool()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeBoolArray() (ret []bool) {
-	ret, err = j.BoolArray()
+	ret, err := j.BoolArray()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeFloat() (ret float64) {
-	ret, err = j.Float()
+	ret, err := j.Float()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeFloatArray() (ret []float64) {
-	ret, err = j.FloatArray()
+	ret, err := j.FloatArray()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeGetBool(keys ...interface{}) (ret bool) {
-	ret, err = j.GetBool(keys...)
+	ret, err := j.GetBool(keys...)
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeGetFloat(keys ...interface{}) (ret float64) {
-	ret, err = j.GetFloat(keys...)
+	ret, err := j.GetFloat(keys...)
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeGetInt(keys ...interface{}) (ret int64) {
-	ret, err = j.GetInt(keys...)
+	ret, err := j.GetInt(keys...)
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeGetString(keys ...interface{}) (ret string) {
-	ret, err = j.GetString(keys...)
+	ret, err := j.GetString(keys...)
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeInt() (ret int64) {
-	ret, err = j.Int()
+	ret, err := j.Int()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeIntArray() (ret []int64) {
-	ret, err = j.IntArray()
+	ret, err := j.IntArray()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeKeys() (ret []string) {
-	ret, err = j.Keys()
+	ret, err := j.Keys()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeLen() (ret int) {
-	ret, err = j.Len()
+	ret, err := j.Len()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeString() (ret string) {
-	ret, err = j.String()
+	ret, err := j.String()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeStringArray() (ret []string) {
-	ret, err = j.StringArray()
+	ret, err := j.StringArray()
 	jgh.PanicOnErr(err)
 	return
 }
 
 func (j Json)  PoeValues() (ret []Json) {
-	ret, err = j.Values()
+	ret, err := j.Values()
 	jgh.PanicOnErr(err)
 	return
 }
